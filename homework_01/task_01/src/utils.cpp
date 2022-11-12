4521dcf74e76776ace6603fbb02bf6d88805f93e
veillon <vasilnikitavasil@gmail.com>
2022-11-12 08:44:44 +0000

    wip

--- homework_01/task_01/src/utils.cpp
+++ homework_01/task_01/src/utils.cpp
@@ -1,7 +1,42 @@
 #include "utils.hpp"
 
-#include <stack>
+#include <string>
+#include <string_view>
+#include <vector>
 
 std::vector<std::string> SplitString(const std::string& data) {
-  return {data};
-}
+  std::vector<std::string> result;
+  std::string el = "";
+  int k = 0;
+  for (char const& letter : data) {
+    if (k == 0) {
+      if (letter == '(') {
+        k = 1;
+      } else if (letter != ' ' and letter != '\t') {
+        el.push_back(letter);
+      } else if (letter != ' ' or letter == '\t') {
+        if (!el.empty()) {
+          result.push_back(el);
+          el.clear();
+        }
+      };
+    };
+    if (k == 1) {
+      if (letter != ')') {
+        el.push_back(letter);
+      } else if (letter == ')') {
+        el.push_back(letter);
+        if (!el.empty()) {
+          result.push_back(el);
+          el.clear();
+        }
+        k = 0;
+      };
+    };
+  };
+  if (!el.empty()) {
+    result.push_back(el);
+    el.clear();
+  }
+  return {result};
+};
--- homework_01/task_02/src/utils.cpp
+++ homework_01/task_02/src/utils.cpp
@@ -1,7 +1,21 @@
 #include "utils.hpp"
 
+#include <iostream>
 #include <stack>
 
 int Calculate(const std::string& data) {
-  return 0;
+  char a1 = data[0];
+  char sign = data[1];
+  char b1 = data[2];
+
+  int a = int(a1);
+  int b = int(b1);
+
+  if (sign == '+') {
+    return a + b;
+  } else if (sign == '-') {
+    return a - b;
+  } else if (sign == '*') {
+    return a * b;
+  }
 }
